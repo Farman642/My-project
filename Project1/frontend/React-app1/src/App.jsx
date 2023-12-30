9695c566aa4a4fc38a439d6b729647288e87b830
Farman <Farmankhan.7874@Gmail.com>
2023-12-30 12:36:13 +0530

    adding frontend

---
+++ Project1/frontend/React-app1/src/App.jsx
@@ -0,0 +1,17 @@
+import Myapp from "./Myapp"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>hello from frontend</h1>
+    <Myapp/>
+    </>
+   
+     
+  )
+}
+
+export default App
---
+++ Project1/frontend/React-app1/src/Myapp.jsx
@@ -0,0 +1,8 @@
+function Myapp(){
+
+    return (
+        <h1>hello from Myapp</h1>
+    )
+}
+
+export default Myapp
